4cbef9909612273d1c40d3444c45057ceb184adb
Kitsune_4460 <116269968+fox0510@users.noreply.github.com>
2026-06-06 19:29:32 +0700

    Delete Base directory

--- Base/dummy_test/lua/autorun/dummy_pm.lua
+++
@@ -1,7 +0,0 @@
-
-	list.Set( "PlayerOptionsModel", "dummy", "models/dummy/collection.mdl" )
-	player_manager.AddValidModel( "dummy", "models/dummy/collection.mdl" )
-        player_manager.AddValidHands( "dummy", "models/weapons/Arms.mdl", 0, "0000000" )	
-
-	AddCSLuaFile( "dummy_pm.lua" )
-
