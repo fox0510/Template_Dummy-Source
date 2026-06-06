74174337ec032c9797ecd89d961acd8f82f737ca
Kitsune_4460 <116269968+fox0510@users.noreply.github.com>
2026-06-06 19:46:48 +0700

    Add files via upload

---
+++ Base/dummy_test/lua/autorun/dummy_pm.lua
@@ -0,0 +1,7 @@
+
+	list.Set( "PlayerOptionsModel", "dummy", "models/dummy/collection.mdl" )
+	player_manager.AddValidModel( "dummy", "models/dummy/collection.mdl" )
+        player_manager.AddValidHands( "dummy", "models/weapons/Arms.mdl", 0, "0000000" )	
+
+	AddCSLuaFile( "dummy_pm.lua" )
+
